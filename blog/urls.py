da4dd948825b788977e24cf1f592a7ecd5c66e51
Arsalan-khalili <arsalankhalili688@gmail.com>
2025-09-04 15:13:29 +0330

    Add public PostListView and PostDetailView with templates; set home to post list; restrict drafts to authors

--- blog/urls.py
+++ blog/urls.py
@@ -2,8 +2,12 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    
     path('signup/',views.SignUpView.as_view(), name='signup'),
     path('profile/',views.ProfileView.as_view(), name='profile'),
-    path('',views.ProfileView.as_view(), name='home'), #just for now. after make a posts list, i change it 
+    path('',views.PostListView.as_view(), name='home'), 
+    path('post/<slug:slug>/', views.PostDetailView.as_view(), name='post_detail'),
+    
+
     
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.views.generic import CreateView, ListView
+from django.views.generic import CreateView, ListView, DeleteView
 from django.urls import reverse_lazy
+from django.db.models import Q
 from .models import Post
 
 # Create your views here.
@@ -21,4 +22,26 @@ class ProfileView(LoginRequiredMixin,ListView):
 
     def get_queryset(self):
         return Post.objects.filter(author = self.request.user ).order_by('-created_at')
-    
+
+class PostListView(ListView):
+    model = Post
+    template_name = 'blog/post_list.html'
+    context_object_name = 'posts'
+    paginate_by = 5
+
+    def get_queryset(self):
+        return (Post.objects.filter(status="published").select_related("author").order_by("-created_at"))
+
+class PostDetailView(DeleteView):
+    model = Post
+    template_name = 'blog/post_detail.html'
+    context_object_name = 'post'
+    slug_field = 'post'
+    slug_url_kwarg = 'slug'
+
+    def get_queryset(self):
+        base_qs = Post.objects.select_related('author')
+        if self.request.user.is_authenticated:
+            return base_qs.filter(Q(status = 'published') | Q(author = self.request.user))
+        return base_qs.filter(status = 'published')
+        
